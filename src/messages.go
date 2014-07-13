4de21493ea2179f0b6d5444e2d3876c3c1739fa5
Dave O <davidobrite@gmail.com>
2014-07-12 19:34:55 -0700

    added unsubscribe, subscribe, and pong messages

--- src/messages.go
+++ src/messages.go
@@ -1,3 +1,29 @@
 package geyser
 
+import (
+	"fmt"
+)
+
+var subscribeCache = make(map[string][]byte)
+var unsubscribeCache = make(map[string][]byte)
+
 var pingMsg = []byte(`{"event":"pusher:ping","data":{}}`)
+var pongMsg = []byte(`{"event":"pusher:pong","data":{}}`)
+
+func subscribe(channelName string) []byte {
+	msg, ok := subscribeCache[channelName]
+	if !ok {
+		msg = []byte(fmt.Sprintf(`{"event":"pusher:subscribe", "data":"{\"channel\":\"%s\"}"}`, channelName))
+		subscribeCache[channelName] = msg
+	}
+	return msg
+}
+
+func unsubscribe(channelName string) []byte {
+	msg, ok := unsubscribeCache[channelName]
+	if !ok {
+		msg = []byte(fmt.Sprintf(`{"event":"pusher:unsubscribe", "data":"{\"channel\":\"%s\"}"}`, channelName))
+		unsubscribeCache[channelName] = msg
+	}
+	return msg
+}
